e40cf6a07b09c329bd097857e7987f9d45e92f2c
Alejandro Arellano <jesusarellano334@gmail.com>
2024-09-24 17:49:34 -0600

    chore: use `as Adapter` for error at prisma :clown_face:

--- auth.ts
+++ auth.ts
@@ -1,8 +1,9 @@
 import { prisma } from "@/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import { Adapter } from "next-auth/adapters";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [],
 });
