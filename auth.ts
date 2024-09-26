8843ffa9705426f259b418cf3d6c6e5cd4008783
Alejandro Arellano <jesusarellano334@gmail.com>
2024-09-26 08:12:54 -0600

    feat: fetch user id at auth callbacks

--- auth.ts
+++ auth.ts
@@ -13,4 +13,31 @@ export const {
   adapter: PrismaAdapter(prisma) as Adapter,
   providers: [Google],
   session: { strategy: "jwt" },
+  callbacks: {
+    async signIn() {
+      // const resp = await loops.createContact(profile?.email!);
+      // if (!resp.success) {
+      //   console.log("LOOPS_ERROR_SIGN_IN_CREATE_CONTACT", resp.message);
+      // }
+      // const customer = await stripe.customers.create({
+      //   email: profile?.email,
+      //   name: profile?.name,
+      // });
+      return true;
+    },
+    async jwt({ token }) {
+      const user = await prisma.user.findUnique({
+        where: { email: token.email! },
+      });
+
+      token.user = user;
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = (token.user as any)?.id;
+      // session.user.customer = (token.user as any)?.customer;
+      // session.user.role = (token.user as any)?.role;
+      return session;
+    },
+  },
 });
